c8ffe4dee973e38838e9042d93d9c5b86b59fc9a
Kim Soo <k.soo@linecorp.com>
2019-08-19 00:24:54 +0900

    hello-world again

--- hello-world/target/debug/deps/hello_world-0125e741beb45c55.d
+++
@@ -1,5 +0,0 @@
-C:\Users\soo\Documents\GitHub\rust-by-example\hello-world\target\debug\deps\hello_world-0125e741beb45c55.exe: src\main.rs
-
-C:\Users\soo\Documents\GitHub\rust-by-example\hello-world\target\debug\deps\hello_world-0125e741beb45c55.d: src\main.rs
-
-src\main.rs:
--- hello-world/target/debug/hello-world.d
+++
@@ -1,1 +0,0 @@
-C:\Users\soo\Documents\GitHub\rust-by-example\hello-world\target\debug\hello-world.exe: C:\Users\soo\Documents\GitHub\rust-by-example\hello-world\src\main.rs
